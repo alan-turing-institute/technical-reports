16a246876c6ac5558c137b6405b97ef662399d5c
James Geddes <jgeddes@turing.ac.uk>
2024-03-07 16:56:44 +0000

    Add s-expression based database

---
+++ bin/publish.scm
@@ -0,0 +1,60 @@
+;; Generate all dynamic parts of the website:
+;; - generate html from copy text
+;; - generate table of reports from database
+
+;; Read database file, validate each record, and produce an internal
+;; representation.  The internal representation is a list of
+;; reports where each report is an association list.
+
+;; True if (map p xs) is a list of #t
+(define (andmap p xs)
+  (if (null? xs)
+      #t
+      (let loop ([xs xs])
+	(cond
+	 [(null? (cdr xs)) (p (car xs))]
+	 [else
+	  (let ([rest (cdr xs)])
+	    (and (p (car xs)) (loop rest)))]))))
+
+(define (list-of-string? xs)
+  (and (list? xs)
+       (andmap string? xs)))
+
+;; A list of integer? of length at most 3
+(define (record-date? xs)
+  (and (list? xs)
+       (or (null? (cdr xs))
+	   (null? (cddr xs))
+	   (null? (cdddr xs)) (length))
+       (andmap integer? xs)))
+
+(define (expect pred?)
+  (λ (field)
+    (if (pred? field)
+	field
+	(raise-exception 'invalid-field-type))))
+
+;; ------------------------------------------------------------
+
+(define *record-type-fields*
+  `((number  . (,(expect integer?)        "integer"))
+    (title   . (,(expect string?)         "string"))
+    (authors . (,(expect list-of-string?) "list of string"))
+    (date    . (,(expect record-date?     "date")))))
+
+;; Parse a record from the database and return an association list
+(define (parse-record-fields fs)
+  fs
+  )
+
+(define (parse-record r)
+  (cond
+   [(and (list? r)
+	 (eq? (car r) 'report))
+    (parse-record-fields (cdr r))]
+   [else
+    (error "Expected a new record.\n" r)]))
+
+;; ------------------------------------------------------------
+
