e924fbb86974a340ff372559d1ae8d5c70dddb3b
James Geddes <jgeddes@turing.ac.uk>
2024-03-25 10:43:37 +0000

    Automate cover sheet generation

--- db/make-cover.rkt
+++ db/make-cover.rkt
@@ -1,16 +1,27 @@
+#! /usr/bin/env racket
 #lang racket/base
 
 ;; Read the reports database (from standard input) and emit a LaTeX
-;; file with the required metadata
+;; file (on standard output) with the required metadata
 
-(require "reports.rkt")
+(require (only-in racket/string string-join)
+         racket/match
+         racket/cmdline
+         "reports.rkt")
 
+
+
+(module+ main
+  (define report-number
+    (command-line #:args (report-number) (string->number report-number)))
+  (make-latex-from-report report-number))
+
+
+;; ------------------------------------------------------------
 
 (define (make-latex-from-report n)
   ;; read-reports reads from standard in by default
-  (define *the-reports* (read-reports))
-
-  (define *the-report* (find-report n))
+  (define *report* (hash-ref (read-reports) n))
   
   (printf #<<EOS
 % This file is a template
@@ -18,17 +29,38 @@
 % from the data in the reports database
 \newcommand{\reportNumber}{~a}
 \newcommand{\reportTitle}{~a}
-\newcommand{\reportAuthors}{Isaac Asimov}
-\newcommand{\reportPublicationDate}{1948}
-\newcommand{\reportCopyrightDate}{2024}
+\newcommand{\reportAuthors}{~a}
+\newcommand{\reportPublicationDate}{~a}
+\newcommand{\reportCopyrightDate}{~a}
 EOS
-          ))
+          (report-number  *report*)
+          (report-title   *report*)
+          (format-authors (report-authors *report*))
+          (format-date    (report-date *report*))
+          (format-date    `(,(car (report-date *report*))))))
+
+(define (format-authors authors)
+  (let ([n (length authors)])
+    (cond
+      [(< n 3) (string-join authors " and ")]
+      [else    (string-join authors ", " #:before-last ", and ")]
+      )))
+
+(define (format-date dt)
+  (match dt
+    [(list yr) (format "~a" yr)]
+    [(list yr mn) (format "~a ~a" (month-of-year mn) yr)]
+    [(list yr mn dy) (format "~a ~a ~a" dy (month-of-year mn) yr)]))
+
+(define (month-of-year mn)
+  (vector-ref *months* (- mn 1)))
+
+(define *months* #("January" "February" "March"
+                   "April" "May" "June"
+                   "July" "August" "September"
+                   "October" "November" "December"))
 
 
 
 
-(module+ main
 
-  (with-input-from-file "db.sexp"
-    (λ () (make-latex-from-report 1)))
-  )
